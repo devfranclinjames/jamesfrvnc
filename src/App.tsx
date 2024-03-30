fd4b04a777587df7d104b9fce7fc33ae618f23d0
devfranclinjames <devfranclinjames@gmail.com>
2024-03-30 15:42:45 +0800

    app layout trial

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import StartBootstrapTemplate from "./components/StartBootstrapTemplate";
 function App() {
   return (
     <>
-      {/* <div id="page-top" data-spy="scroll" data-target="#spyThisNav" data-offset="0"></div> */}
-      <StartBootstrapTemplate />
+      <StartBootstrapTemplate></StartBootstrapTemplate>
     </>
   );
 }
