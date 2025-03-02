2bd18a7dc63b5f0a60c0e7a1b2f4f8d446d07e8b
devfranclinjames <devfranclinjames@gmail.com>
2025-03-03 07:17:59 +0800

    adding test page + reactrouter

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -5,10 +5,8 @@ export default function Test() {
         width="100%"
         height="1080px"
         src="https://miro.com/app/embed/uXjVIXQc2nw=/?pres=1&frameId=3458764619273613184&embedId=807032826873"
-        frameborder="0"
         scrolling="no"
         allow="fullscreen; clipboard-read; clipboard-write"
-        allowfullscreen
       ></iframe>
     </>
   );
